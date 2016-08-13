46b32808bc859085f40370bc6aed310118ceb8a6
Nihar Sarkar <NIHAR-SARKAR@users.noreply.github.com>
2016-08-13 16:42:51 +0530

    Update script.js

--- ang-ng-repeat/js/script.js
+++ ang-ng-repeat/js/script.js
@@ -11,12 +11,12 @@
 			address:"new town",
 			marks:[{it:"90"},{it:"75"},{it:"97"}]
 		},{
-			name:"mita",
+			name:"Rajesh",
 			id:"124",
 			address:"new town",
 			marks:[{it:"94"},{it:"65"},{it:"82"}]
 		},{
-			name:"ishita",
+			name:"Mithun",
 			id:"125",
 			address:"new town",
 			marks:[{it:"91"},{it:"68"},{it:"87"}]
